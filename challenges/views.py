d344bb4b8d353da45bf801fabc95978d8dc56284
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-17 10:58:38 +0000

    update file with redirect and reverse function

--- challenges/views.py
+++ challenges/views.py
@@ -1,43 +1,45 @@
-from django.http.response import HttpResponse
-from django.shortcuts import render
+from django.http.response import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http.request import HttpRequest
+from django.urls import reverse
+from django.shortcuts import redirect, render
 
 # Create your views here.
 monthly_challenges = {
-    "january": "go for a walk atleast 20min",
-    "february": "learn django",
-    "march": "qwerty",
-    "april": "suyash",
-    "march": "mahajan",
+    "january": "go for walk atleast 20min",
+    "february": "learn django for atleat 1 hour ",
+    "march": "learn web development",
+    "april": "Go for a drive",
     "may": "go for a walk atleast 20min",
-    "june": "learn django",
-    "july": "qwerty",
+    "june": "celebrate birthday",
+    "july": "learn django",
     "august": "suyash",
     "september": "mahajan",
 }
 
 
 def monthly_challenge_by_number(request, month):
-    return HttpResponse(month)
+    if month <= len(monthly_challenges):
+        months = list(monthly_challenges.keys())
+        redirect_month = months[month-1]
+        redirect_path =  reverse(monthly_challenge, args=[redirect_month]) #will give the path eg /challenges/january
+        return HttpResponseRedirect(redirect_path)
+    else:
+        return HttpResponseNotFound("Invaid Month")
 
 
 def monthly_challenge(request, month):
-    if month == "january":
-        return HttpResponse("jan")
-    elif month == "february":
-        return HttpResponse("feb")
-    elif month == "march":
-        return HttpResponse("march")
-    elif month == "april":
-        return HttpResponse("april")
-    elif month == "may":
-        return HttpResponse("may")
-    elif month == "june":
-        return HttpResponse("june")
-    elif month == "july":
-        return HttpResponse("july")
-    elif month == "august":
-        return HttpResponse("august")
-    elif month == "september":
-        return HttpResponse("sept")
-    else:
-        return HttpResponse(f"MonthError")
+    try:
+        return HttpResponse(f"<h1>{monthly_challenges[month]}</h1>")
+    except KeyError:
+        return HttpResponseNotFound("<h1>Month not found</h1>")
+
+def months_list(request):
+    list_item = ""
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        month_link = reverse(monthly_challenge, args=[month])
+        list_item += f'<li><a href="{month_link}">{month.capitalize()}</a></li>'
+     
+    response_data = f"<ul>{list_item}</ul>"
+    return HttpResponse(response_data)
