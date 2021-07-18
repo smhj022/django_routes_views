69a308c875880edd6b3b8b97409f92bd4b3aab07
Ubuntu <ubuntu@ip-172-31-30-172.ap-south-1.compute.internal>
2021-07-18 13:20:59 +0000

    templates inheritannce, Partial templates snippets

--- challenges/views.py
+++ challenges/views.py
@@ -2,6 +2,7 @@ from django.http.request import HttpRequest
 from django.http.response import (HttpResponse, HttpResponseNotFound,
                                   HttpResponseRedirect)
 from django.shortcuts import redirect, render
+from django.template.loader import render_to_string
 from django.urls import reverse
 
 # Create your views here.
@@ -22,7 +23,6 @@ monthly_challenges = {
 
 def index(request):
     months = list(monthly_challenges.keys())
-    print(months)
     return render(request, "challenges/index.html", {"months_name": months})
 
 
@@ -33,7 +33,8 @@ def monthly_challenge_by_number(request, month):
         redirect_path = reverse(monthly_challenge, args=[redirect_month])  # will give the path eg /challenges/january
         return HttpResponseRedirect(redirect_path)
     else:
-        return HttpResponseNotFound("Invaid Month")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
 
 
 def monthly_challenge(request, month):
@@ -41,4 +42,5 @@ def monthly_challenge(request, month):
         month_challenge = monthly_challenges[month]
         return render(request, "challenges/challenge.html", {"text": month_challenge, "month_name": month.capitalize()})
     except Exception:
-        return HttpResponseNotFound("<h1>Month not found</h1>")
+        response_data = render_to_string("404.html")
+        return HttpResponseNotFound(response_data)
